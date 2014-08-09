03559e9ef0e8b2f471a5a6caa73e3832a94d7e1f
aahz <and_ilush@mail.ru>
2014-08-09 05:13:09 +0400

    License issues.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
+/**
+ Copyright (c) Andrew Ozdion aka Aahz
+
+ This file is part of vvvvvv2.
+
+ vvvvvv2 is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ vvvvvv2 is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 ;(function (window, $, undefined) {"use strict";
 
     window._vvvvvv = {};
